a68ce3cdf9170282f956494dac0b389b92bb5d5d
Sara Marino <saradaenaamarino@gmail.com>
2018-12-02 11:48:14 -0500

    added in locations and tour dates.

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+var members = ["Dave Grohl", "Nate Mendel", "Pat Smear", "Taylor Hawkins", "Rami Jaffee", "Chris Shiflett"];
+document.getElementById("xyz").innerHTML = members;
+
+var tour = {
+  Chicago: ["July 29 2018", "July 30 2018"],
+  Seattle: ["September 1 2018"],
+  Edmonton: ["September 4 2018"],
+};
